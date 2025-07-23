3beab07aa831bc22c3fb46fd87b7ec2ae3b40f30
1luskin <youremail@example.com>
2025-07-23 12:43:02 +0300

    fix: added react-icons and replaced broken icon

--- components/shader/index.ts
+++ components/shader/index.ts
@@ -1,8 +1,9 @@
 export { Container } from "./container";
 export { Header } from "./header";
 export { Title } from "./title";
-export { Catigories } from "./categories";
+export { Categories } from "./categories";
 export { SortPopup } from "./sort-popup";
 export { TopBar } from "./top-bar";
 export { FilterCheckbox } from "./filtercheckbox";
 export { Filters } from "./filter";
+import { SiPostgresql } from "react-icons/si";
